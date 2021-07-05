67c1b6bb0b66a925c37625fc3a5afcf5b924808d
pumba <nompumelelomasilela23@gmail.com>
2021-07-05 11:01:10 +0200

    chnages and map APi

---
+++ Scripts/GooglemapAPI.js
@@ -0,0 +1,10 @@
+function initMap(){
+
+
+    var options = {
+        center: {lat: 26.1074, lng: 28.0548,},
+        zoom: 8 
+    }
+
+    Map = new google.maps.Map(document. getElementById("locationMap"), options)
+}
